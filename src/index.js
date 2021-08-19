e72a417e2e030185e04799078a5e8b3210cb6c28
avshir <av.shirinskaya@gmail.com>
2021-08-19 16:28:56 +0300

    feat: add solution. Test 100%

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let arrExpr = [];
+	while (expr.length > 0) {
+		arrExpr.push(expr.slice(0, 10));
+		expr = expr.slice(10);
+	}
+	//console.log(arrExpr);//["0000000010", "0000111010", "1010101111", "0011101110", "0000001011", "1111101010", "1111101010", "1010101010"]
+	for (let i = 0; i < arrExpr.length; i++) {
+		arrExpr[i] = arrExpr[i].replace(/10/g, '.').replace(/11/g, '-').replace(/00/g, '');
+	}
+	//console.log(arrExpr);//[".", "-..", "...--", "-.-.", ".-", "--...", "--...", "....."]
+	let resStr = '';
+	for (let i = 0; i < arrExpr.length; i++){
+		if (arrExpr[i] === '**********') {
+			resStr += ' ';
+		} else {
+			resStr += MORSE_TABLE[arrExpr[i]];
+		}
+	}
+	//console.log(resStr);
+	return resStr;
 }
 
 module.exports = {
